61475cc47d363281599f0859f91246932f5d811a
Mikhail Timofeev <mikhail.timofeev@researchstudio.at>
2018-11-15 10:22:13 +0100

    added stuff to gitignore

--- flann-1.6.11-src/build/doc/CMakeFiles/auxclean.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/auxclean"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/auxclean.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
